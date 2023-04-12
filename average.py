6cd1e1b2593e03ce48b02f704d41b2f1a35f92b8
Jstorto21 <stortoj@oregonstate.edu>
2023-04-12 13:56:34 -0400

    Asking user to enter 5 numbers and find and display the average.

---
+++ average.py
@@ -0,0 +1,15 @@
+# Author: Joe Storto
+# Github Username: Jstorto21
+# Date: 4/12/2023
+# This a program asks for five numbers and then prints out the average of those numbers
+
+print("Please enter five numbers.")
+num_1 = float(input())    #creating input for numbers 1-5
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+addition = (num_1 + num_2 + num_3+ num_4 + num_5) #variable to find total sum of numbers
+
+print("the average of those numbers is:", addition / 5)  #print the total sum divided by 5
+
