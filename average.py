044a544ee7e112f7f119db62d200051054573bf6
Jstorto21 <stortoj@oregonstate.edu>
2023-04-12 15:05:08 -0400

    Asking user to enter 5 numbers and find and display the average. Add // for rounded. 3.0

--- average.py
+++ average.py
@@ -4,12 +4,12 @@
 # This a program asks for five numbers and then prints out the average of those numbers
 
 print("Please enter five numbers.")
-num_1 = float(input())    #creating input for numbers 1-5
+num_1 = float(input())                              #input or all 5 numbers
 num_2 = float(input())
 num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
-average = ((num_1 + num_2 + num_3+ num_4 + num_5) // 5) #variable to find total sum of numbers
+average = ((num_1 + num_2 + num_3+ num_4 + num_5)/5) #variable to find total sum of numbers
 
-print("the average of those numbers is:", average)  #print the total sum divided by 5
+print("the average of those numbers is:",average)  #print the total sum divided by 5
 
